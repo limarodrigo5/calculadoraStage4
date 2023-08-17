881718a11318eb54b6d7751d558aacc6a27e58cf
limarodrigo5 <limarodrigobr@gmail.com>
2023-08-17 14:51:30 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Calculadora de Operações Básicas 🧮
+
+Bem-vindo à Calculadora de Operações Básicas! Este é um projeto em JavaScript que permite realizar operações matemáticas simples entre dois números inseridos pelo usuário.
+
+## Conteúdos Abordados 📚
+
+- Interação com o usuário
+- Uso de loops para validação
+- Utilização de condicionais para análise adicional
+- Definição de funções para operações matemáticas
+
+## Objetivo 🎯
+
+O objetivo desta calculadora é proporcionar uma ferramenta para realizar operações matemáticas fundamentais de forma rápida e prática. Você poderá realizar somas, subtrações, multiplicações e divisões, além de verificar a paridade da soma dos números e a igualdade entre eles.
+
+## Como Utilizar 🖩
+
+1. Abra o arquivo `index.html` em um navegador web.
+2. Insira dois números nos campos designados.
+3. Selecione a operação que deseja realizar.
+4. Os resultados e análises serão exibidos em alertas.
+
+## Funcionalidades 🔧
+
+- Soma ➕
+- Subtração ➖
+- Multiplicação ✖️
+- Divisão ➗
+- Resto da Divisão ➡️
+- Verificação de paridade da soma dos números 🔍
+- Identificação de igualdade entre os números 🔁
+
+## Resultados e Análises 📊
+
+Após selecionar uma operação, você receberá alertas exibindo os resultados correspondentes. Além disso, a calculadora irá informar se a soma dos números é par ou ímpar, bem como se os números são iguais ou diferentes.
+
+## Exemplo de Uso 🌟
+
+- Números inseridos: 5 e 7
+- Operação selecionada: Soma
+- Resultado: A soma entre 5 e 7 é: 12
+
+## Próximos Passos 🚀
+
+Este projeto pode ser expandido e melhorado de diversas maneiras:
+- Estilização para uma melhor apresentação visual.
+- Tratamento de erros para garantir entrada válida.
+- Inclusão de mais operações matemáticas avançadas.
+
+Sinta-se à vontade para personalizar e aprimorar este projeto de acordo com suas necessidades e conhecimentos!
+
+---
+
+Desenvolvido por Rodrigo Lima 💼
---
+++ script.js
@@ -0,0 +1,81 @@
+/* Nesse desafio, você irá solicitar ao usuário que ele insira **dois números** e, a partir daí, calcular:
+
+- [ ]  A soma dos dois números;
+- [ ]  A subtração dos dois números;
+- [ ]  A multiplicação dos dois números;
+- [ ]  A divisão dos dois números;
+- [ ]  O resto da divisão dos dois números;
+
+Fácil até aqui, né? Bora se desafiar? 👀 🧑‍🚀
+
+- [ ]  Verifique se a soma dos dois números é par ~~(ou ímpar)~~;
+- [ ]  Verifique se os dois números inseridos são iguais ~~(ou diferentes)~~. */
+
+let num1 = Number(prompt("Digite um número: "));
+let num2 = Number(prompt("Digite outro número: "));
+
+// criar um alerta se o usuário não digitar um número e retornar para o início
+while (isNaN(num1) || isNaN(num2)) {
+  alert("Você não digitou um número em uma das caixinhas. Tente novamente.");
+  num1 = Number(prompt("Digite um número: "));
+  num2 = Number(prompt("Digite outro número: "));
+}
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function subtraction(num1, num2) {
+  return num1 - num2;
+}
+
+function multiplication(num1, num2) {
+  return num1 * num2;
+}
+
+function division(num1, num2) {
+  return num1 / num2;
+}
+
+function rest(num1, num2) {
+  return num1 % num2;
+}
+
+function sumIsEven(num1, num2) {
+  if ((num1 + num2) % 2 === 0) {
+    return "A soma dos dois números é par.";
+  } else {
+    return "A soma dos dois números é ímpar.";
+  }
+}
+
+function isEquals(num1, num2) {
+  if (num1 === num2) {
+    return "Os dois números são iguais.";
+  } else {
+    return "Os dois números são diferentes.";
+  }
+}
+
+alert(`
+A soma entre ${num1} e ${num2} é: ${sum(num1, num2)}
+`);
+
+alert(`
+A subtração entre ${num1} e ${num2} é: ${subtraction(num1, num2)}
+`);
+
+alert(`
+A multiplicação entre ${num1} e ${num2} é: ${multiplication(num1, num2)}
+`);
+
+alert(`
+A divisão entre ${num1} e ${num2} é: ${division(num1, num2)}
+`);
+
+alert(`
+O resto da divisão entre ${num1} e ${num2} é: ${rest(num1, num2)}
+`);
+
+alert(sumIsEven(num1, num2));
+alert(isEquals(num1, num2));
